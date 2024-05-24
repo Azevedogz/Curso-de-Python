69a271adde4b23c658ea9d84503a0759471c8a09
Azevedogz <guguazevedo90@gmail.com>
2024-05-24 11:31:45 -0300

    version

---
+++ app.py
@@ -0,0 +1,6 @@
+# Estou fazendo esse curso de Python para desenvolver minhas habilidades e
+# me qualificar para o mercado de trabalho
+# espero que goste da minha evolucao a partir daqui
+
+
+print('Seja bem vindo!')
